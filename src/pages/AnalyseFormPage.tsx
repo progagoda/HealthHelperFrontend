8f030e30de5551ce3164ee888c7cf6cdb798d326
Brudar Artem Sergeevich <71312371+progagoda@users.noreply.github.com>
2023-12-29 23:18:30 +0300

    add theme to form page

--- src/pages/AnalyseFormPage.tsx
+++ src/pages/AnalyseFormPage.tsx
@@ -1,28 +1,54 @@
 import React, { FC } from 'react';
 import { useSelector } from 'react-redux';
 import { TAnalysesState } from '../types';
-import { DatePicker, Form, InputNumber, Typography } from 'antd';
+import {
+  ConfigProvider,
+  DatePicker,
+  Form,
+  InputNumber,
+  theme,
+  Typography,
+} from 'antd';
+import { useThemeParams } from '@vkruglikov/react-telegram-web-app';
 
 const { Title } = Typography;
 export const AnalyseFormPage: FC = () => {
   const selectedAnalyse = useSelector(
     (state: TAnalysesState) => state.selectedAnalyse,
   );
-
+  const [colorScheme, themeParams] = useThemeParams();
   return (
-    <div style={{ marginLeft: 5 }}>
-      <Title level={3}>{selectedAnalyse.name}</Title>
-      <Form
-        labelCol={{ span: 4 }}
-        wrapperCol={{ span: 14 }}
-        layout="horizontal"
-      ></Form>
-      <Form.Item label="Value: ">
-        <InputNumber />
-      </Form.Item>
-      <Form.Item label="Date: ">
-        <DatePicker />
-      </Form.Item>
-    </div>
+    <ConfigProvider
+      theme={
+        themeParams.text_color
+          ? {
+              algorithm:
+                colorScheme === 'dark'
+                  ? theme.darkAlgorithm
+                  : theme.defaultAlgorithm,
+              token: {
+                colorText: themeParams.text_color,
+                colorPrimary: themeParams.button_color,
+                colorBgBase: themeParams.bg_color,
+              },
+            }
+          : undefined
+      }
+    >
+      <div style={{ marginLeft: 5 }}>
+        <Title level={3}>{selectedAnalyse.name}</Title>
+        <Form
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 14 }}
+          layout="horizontal"
+        ></Form>
+        <Form.Item label="Value: ">
+          <InputNumber />
+        </Form.Item>
+        <Form.Item label="Date: ">
+          <DatePicker />
+        </Form.Item>
+      </div>
+    </ConfigProvider>
   );
 };
