c5ff23c6702f20241b137a613215abf86496a5b3
Brudar Artem Sergeevich <71312371+progagoda@users.noreply.github.com>
2023-12-30 16:25:31 +0300

    add theme in form page

--- src/pages/AnalyseFormPage.tsx
+++ src/pages/AnalyseFormPage.tsx
@@ -1,14 +1,26 @@
 import React, { FC, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { TAnalysesState } from '../types';
-import { DatePicker, Form, InputNumber, Typography } from 'antd';
-import { BackButton, MainButton } from '@vkruglikov/react-telegram-web-app';
+import {
+  ConfigProvider,
+  DatePicker,
+  Form,
+  InputNumber,
+  theme,
+  Typography,
+} from 'antd';
+import {
+  BackButton,
+  MainButton,
+  useThemeParams,
+} from '@vkruglikov/react-telegram-web-app';
 import { useNavigate } from 'react-router-dom';
 import dayjs from 'dayjs';
 import { addAnalyse } from '../store';
 
 const { Title } = Typography;
 export const AnalyseFormPage: FC = () => {
+  const [colorScheme, themeParams] = useThemeParams();
   const selectedAnalyse = useSelector(
     (state: TAnalysesState) => state.selectedAnalyse,
   );
@@ -36,21 +48,39 @@ export const AnalyseFormPage: FC = () => {
   };
 
   return (
-    <div style={{ marginLeft: 5 }}>
-      <Title level={3}>{selectedAnalyse.name}</Title>
-      <Form
-        labelCol={{ span: 4 }}
-        wrapperCol={{ span: 14 }}
-        layout="horizontal"
-      ></Form>
-      <Form.Item label="Value: ">
-        <InputNumber onChange={onChangeValue} />
-      </Form.Item>
-      <Form.Item label="Date: ">
-        <DatePicker onChange={(value) => setDate(value)} />
-      </Form.Item>
-      {value && date && <MainButton text={'Add to card'} onClick={submit} />}
-      <BackButton onClick={goBack} />
-    </div>
+    <ConfigProvider
+      theme={
+        themeParams.text_color
+          ? {
+              algorithm:
+                colorScheme === 'dark'
+                  ? theme.darkAlgorithm
+                  : theme.defaultAlgorithm,
+              token: {
+                colorText: themeParams.text_color,
+                colorPrimary: themeParams.button_color,
+                colorBgBase: themeParams.bg_color,
+              },
+            }
+          : undefined
+      }
+    >
+      <div style={{ marginLeft: 5 }}>
+        <Title level={3}>{selectedAnalyse.name}</Title>
+        <Form
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 14 }}
+          layout="horizontal"
+        ></Form>
+        <Form.Item label="Value: ">
+          <InputNumber onChange={onChangeValue} />
+        </Form.Item>
+        <Form.Item label="Date: ">
+          <DatePicker onChange={(value) => setDate(value)} />
+        </Form.Item>
+        {value && date && <MainButton text={'Add to card'} onClick={submit} />}
+        <BackButton onClick={goBack} />
+      </div>
+    </ConfigProvider>
   );
 };
