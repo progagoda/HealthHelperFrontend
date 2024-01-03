0da828e21e846e0e7980dd1477a6cb2e14c64e7c
Brudar Artem Sergeevich <71312371+progagoda@users.noreply.github.com>
2024-01-03 17:28:11 +0300

    switch to constants.db

--- src/pages/AnalysisListPage.tsx
+++ src/pages/AnalysisListPage.tsx
@@ -1,16 +1,14 @@
 import { List } from 'antd';
 import { TAnalyse } from '../types';
 import { AnalyseCard } from '../components/analyse-card/AnalyseCard';
-import { analysisAPI } from '../api';
 import { useSelectorAnalysis } from '../hooks/storeHooks';
 import { MainButton } from '@vkruglikov/react-telegram-web-app';
 import _ from 'lodash';
 import { useTelegram } from '../hooks/useTelegram';
+import { ANALYSIS } from '../constants';
 
 export const AnalysisListPage = () => {
-  const { data: analysis } = analysisAPI.useGetAnalysisDictionaryQuery('');
   const selectedAnalysis = useSelectorAnalysis();
-  console.log(selectedAnalysis);
   const { tg } = useTelegram();
 
   return (
@@ -18,7 +16,7 @@ export const AnalysisListPage = () => {
       <List
         size={'small'}
         grid={{ column: 3 }}
-        dataSource={analysis}
+        dataSource={ANALYSIS}
         renderItem={(analyse: TAnalyse) => (
           <List.Item>
             <AnalyseCard analyse={analyse} />
