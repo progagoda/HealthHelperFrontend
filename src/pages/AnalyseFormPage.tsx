db3519abb63edbff0cd7ab337204162637bf9b40
Brudar Artem Sergeevich <71312371+progagoda@users.noreply.github.com>
2024-01-04 17:54:24 +0300

    testing user.id 3

--- src/pages/AnalyseFormPage.tsx
+++ src/pages/AnalyseFormPage.tsx
@@ -69,6 +69,7 @@ export const AnalyseFormPage: FC = () => {
     >
       <div style={{ marginLeft: 5 }}>
         <Title level={3}>{selectedAnalyse.name}</Title>
+        {initDataUnsafe?.user?.id}
         <Form
           labelCol={{ span: 4 }}
           wrapperCol={{ span: 14 }}
